43d60dc26a105b1a9c7c231a99cab6b83f0f2213
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-02-14 16:06:24 +0300

    new functionaly added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 import "./index.scss"
 
 function App() {
-  const [ timeLeft, setTimeLeft ] = useState(25 * 60);
+  const [ times, setTimes ] = useState(25);
+  const [ timeLeft, setTimeLeft ] = useState(times * 60);
   const [ isCounting, setIsCounting ] = useState(false);
 
   const getPadTime = time => time.toString().padStart(2, "0"); 
 
-  const minutes = getPadTime(Math.floor(timeLeft / 60));
-  const seconds = getPadTime(timeLeft - minutes * 60);
-
+  let minutes = getPadTime(Math.floor(timeLeft / 60));
+  let seconds = getPadTime(timeLeft - minutes * 60);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -19,22 +19,37 @@ function App() {
     return () => {
       clearInterval(interval);
     }
-  }, [isCounting])
- 
+  }, [isCounting]);
 
+  useEffect(() => {
+    setTimeLeft(() => times * 60);
+  }, [times])
+ 
   const handleStart = () => {
     setIsCounting(!isCounting);
   };
   const handleReset = () => {
-    setTimeLeft(25 * 60);
+    setTimeLeft(times * 60);
     setIsCounting(false);
   };
 
+  const changeTime = (e) => {
+    e.preventDefault();
+    setTimes(() => e.target.value);
+    // setTimeLeft(() => times * 60);
+  }
+  
+  
   return (
     <div className="App">
-      <div className='timer'>
-         
-      </div>
+        <form action="#" className='timer'>
+          <select  onChange={(e) => changeTime(e)}>
+            <option value={25}>25 min</option>
+            <option value={35}>35 min</option>
+            <option value={45}>45 min</option>
+            <option value={60}>60 min</option>
+          </select>
+        </form>
       <div className="container">
         <h1>{minutes} : {seconds}</h1>
       </div>
